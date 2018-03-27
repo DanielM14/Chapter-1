4a55b6c0df0212f562f33fdd3b59fe9fa744b980
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-03-27 13:06:59 -0500

    update

---
+++ ExpensiveCalculater/ExpensiveCalculater/Source.cpp
@@ -0,0 +1,24 @@
+//Expensive Calculater
+//3/27/2018
+//Author: Daniel McGlasson
+
+
+#include<iostream>
+
+int main()
+{
+	std::cout << "7 + 3 = " << 7 + 3 << std::endl;
+	std::cout << "7 - 3 = " << 7 - 3 << std::endl;
+	std::cout << "7 x 3 = " << 7 / 3 << std::endl;
+	std::cout << "7.0 / 3.0 = " << 7.0 / 3.0 << std::endl;
+	std::cout << "7 % 3 = " << 7 % 3 << std::endl;
+	
+	std::cout << "7 + 3 x 5 = " << 7 + 3 * 5 << std::endl;
+	std::cout << "(7+3) x 5 =" << (7 + 3) * 5 << std::endl;
+	system("Pause");
+	return 0;
+
+
+
+
+}
